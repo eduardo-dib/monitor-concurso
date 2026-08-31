832bee242110e1cdd76637c809734d523fe53660
eduardo-dib <cardozoedudib@gmail.com>
2026-08-31 19:55:10 -0300

    fix: diferencia ambientes para envio de email de redefinicao de senha

--- monitor.concurso/src/main/java/dev/eduardodib/service/email/RecuperacaoSenhaService.java
+++ monitor.concurso/src/main/java/dev/eduardodib/service/email/RecuperacaoSenhaService.java
@@ -5,25 +5,31 @@ import io.quarkus.mailer.Mailer;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @ApplicationScoped
 public class RecuperacaoSenhaService {
 
     @Inject
     Mailer mailer;
 
+    @ConfigProperty(name = "app.frontend.url")
+    String frontendUrl;
+
     public void enviarEmailRecuperacao(String emailDestino, String token) {
-
-        String link = "http://localhost:3000/redefinir-senha?token=" + token;
-
+        String link = frontendUrl + "/redefinir-senha?token=" + token;
         String html = """
-            <h2>Recuperação de Senha - Monitor de Concursos</h2>
+            <h2>Recuperação de Senha - VigiaConcursos</h2>
             <p>Você solicitou a redefinição de senha da sua conta.</p>
             <p>Clique no link abaixo para criar uma nova senha. Este link expira em 1 hora.</p>
             <a href="%s">Redefinir minha senha</a>
             <br/><br/>
             <p>Se você não solicitou, ignore este e-mail.</p>
         """.formatted(link);
-
         mailer.send(Mail.withHtml(emailDestino, "Redefinição de Senha", html));
     }
 }
